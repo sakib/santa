cf55c5661bcba9740689731bf12544a77eb0f2f5
sakibj <sakib.jalal@gmail.com>
2014-12-13 00:15:47 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Secret Santa 2014 Flask App by Sakib Jalal
+Uses MongoDB for database storage and a text file for backup
+
---
+++ run.py
@@ -0,0 +1,38 @@
+from flask import Flask, request, render_template, redirect
+import pymongo
+from pymongo import MongoClient
+
+app = Flask(__name__)
+
+@app.route('/', methods=['GET'])
+def main_page():	
+	return render_template('form.html')
+
+@app.route('/form', methods=['POST'])
+def add_to_database():
+	fname 	= request.form['firstname']	
+	lname 	= request.form['lastname']
+	email 	= request.form['email']
+	info 	= request.form['info']
+	f 	= open('storage.txt', 'a')
+	client 	= MongoClient()
+	db 	= client.sakib
+	if (db.santa.find({"email": email}).count() == 0):
+		f.write(fname + "|" + lname + "|" + email + "|" + info + "\n")
+		db.santa.insert({'fname':fname, 'lname':lname, 'email':email, 'info':info})
+	else:
+		return redirect('invalid', 301)	
+	
+	f.close()
+	return redirect('/winner', 301)
+
+@app.route('/winner')
+def success():
+	return render_template('worked.html')
+
+@app.route('/invalid')
+def invalid():
+	return render_template('invalid.html')
+
+if __name__ == '__main__':
+	app.run(debug=True)
